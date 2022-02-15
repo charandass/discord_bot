6317ee44b55da43129ac7f22d77347b4a4dbd557
CHARAN DASS <43849626+charandass@users.noreply.github.com>
2022-02-15 11:52:15 +0000

    adding some extra commands

--- extracode.py
+++ extracode.py
@@ -1,8 +1,10 @@
+
 # from discord.ext import commands
 # from discord import guild
 # from discord_slash import SlashCommand, SlashContext
 # from discord_slash.utils.manage_commands import create_choice, create_option
 
+# client = discord.Client()
 
 # client  = commands.Bot(command_prefix='!')
 # slash = SlashCommand(client, sync_commands=True)
@@ -24,3 +26,28 @@
 
 # async def _Hello(ctx:SlashContext,):
 #     await ctx.send("Hi there")
+
+
+
+
+
+# @client.event
+# async def on_ready():
+#   print('We have logged in as {0.user}'.format(client))
+
+
+# @client.event
+# async def on_message(message):
+#   if message.author == client.user:
+#     return
+
+#   msg = message.content
+
+#   if msg.startswith("$hi"):
+#     await message.channel.send("hello")
+  
+#   if msg.startswith("$TodaySales"):
+#     await message.channel.send(450000)
+
+#   if msg.startswith("$WeeklySales"):
+#     await message.channel.send(9800000)
--- main.py
+++ main.py
@@ -3,30 +3,54 @@ from webserver import keep_alive
 import os
 import discord 
 from discord.ext import commands
+from discord import guild
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
 
-client = discord.Client()
+guild_id = [942720087687839754]
+
+
 client  = commands.Bot(command_prefix='!')
+slash = SlashCommand(client, sync_commands=True)
 
-@client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+@slash.slash(
+    name= "Hi",
+    description="Just send a message",
+    guild_ids = guild_id
+)
+async def _Hello(ctx:SlashContext,):
+    await ctx.send("Hi there")
+
+@slash.slash(
+  name='todaysales',
+  description="Tells you the today's sales ",
+  guild_ids = guild_id
+)
+
+async def _TodaySales(ctx:SlashContext):
+  await ctx.send(4560000)
+
+@slash.slash(
+  name='weeklysales',
+  description="Tells you the stoday",
+  guild_ids = guild_id
+)
+
+async def _WeeklySales(ctx:SlashContext):
+  await ctx.send(98000000)
 
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+@slash.slash(
+  name='balance',
+  description="Tells you the stoday",
+  guild_ids = guild_id
+)
 
-  msg = message.content
+async def _Balance(ctx:SlashContext):
+  await ctx.send(7800000000)
 
-  if msg.startswith("$hi"):
-    await message.channel.send("hello")
-  
-  if msg.startswith("$TodaySales"):
-    await message.channel.send(450000)
 
-  if msg.startswith("$WeeklySales"):
-    await message.channel.send(9800000)
+
 
 
 keep_alive()
@@ -34,6 +58,11 @@ keep_alive()
 TOKEN = os.environ.get("DISCORD_BOT_SECRET")
 
 client.run(TOKEN)
+
+
+
+
+
 
 
 
