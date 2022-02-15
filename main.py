e3d3f808fb4200defd3256e96240f47f1a8e9954
CHARAN DASS <43849626+charandass@users.noreply.github.com>
2022-02-15 10:33:37 +0000

    shifting code

--- main.py
+++ main.py
@@ -36,29 +36,4 @@ TOKEN = os.environ.get("DISCORD_BOT_SECRET")
 client.run(TOKEN)
 
 
-# from discord.ext import commands
-# from discord import guild
-# from discord_slash import SlashCommand, SlashContext
-# from discord_slash.utils.manage_commands import create_choice, create_option
 
-
-# client  = commands.Bot(command_prefix='!')
-# slash = SlashCommand(client, sync_commands=True)
-
-# @slash.slash(
-#     name= "Hi",
-#     description="Just send a message",
-#     guild_ids = [942720087687839754]
-# )
-
-# @slash.slash(
-#   name='sales',
-#   description="Tells you the sales today",
-#   guild_ids = [942720087687839754]
-# )
-
-# async def _Sales(ctx:SlashContext):
-#   await ctx.send(4560000)
-
-# async def _Hello(ctx:SlashContext,):
-#     await ctx.send("Hi there")
